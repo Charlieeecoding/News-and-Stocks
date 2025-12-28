51d84922e2ab70efd9fc1e924729ea4a6e79e1ae
Charlie Yung <bogboglong@gmail.com>
2025-12-29 00:37:06 +0800

    extraction testing

--- dag/news_pipeline.py
+++ dag/news_pipeline.py
@@ -1,9 +1,44 @@
+import requests
 import os
+from dotenv import load_dotenv
+import logging
+import json
 
-conn = psycopg2.connect(
-    dbname=os.getenv('POSTGRES_DB'),
-    user=os.getenv('POSTGRES_USER'),
-    password=os.getenv('POSTGRES_PASSWORD'),
-    host='news-and-stocks',  # container name
-    port=5432
-)
+log = logging.getLogger(__name__)
+
+# importing information from .env file
+load_dotenv()
+
+# Define constants for API access
+NEWS_API_URL = os.getenv('NEWS_API_URL')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+
+# fetching news data 
+# def fetch_news(news_url, api_key, query, language):
+#     params = {
+#         'apiKey': api_key,
+#         'q': query,
+#         'language': language,
+#     }
+#     response = requests.get(news_url, params=params)
+#     response.raise_for_status()
+#     return response.json()
+
+api_url = f"{NEWS_API_URL}{NEWS_API_KEY}&language=en&pageSize=5"
+
+def fetch_news():
+    response = requests.get(api_url)
+    response.raise_for_status()
+    return response.json()
+
+# fetch_news({NEWS_API_URL}, {NEWS_API_KEY}, 'IBM', 'en')
+
+if __name__ == "__main__":
+    try:
+        news_data = fetch_news()
+        log.info("Fetched news data successfully.")
+        pretty = json.dumps(news_data, indent=2)
+        print(pretty)
+        log.info(pretty)
+    except Exception as e:
+        log.error(f"Error fetching news data: {e}")
