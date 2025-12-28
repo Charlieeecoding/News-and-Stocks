dbbd09bea1d63348cdee14057608f62cd64e8ed8
Charlie Yung <bogboglong@gmail.com>
2025-12-28 23:01:50 +0800

    migrated to Ubuntu-24 environment

---
+++ dag/news_pipeline.py
@@ -0,0 +1,9 @@
+import os
+
+conn = psycopg2.connect(
+    dbname=os.getenv('POSTGRES_DB'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    host='news-and-stocks',  # container name
+    port=5432
+)
