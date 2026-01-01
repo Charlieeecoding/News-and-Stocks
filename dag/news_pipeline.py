d497ec8b170ac65dbb8904c447dab1b0df618325
Charlie Yung <bogboglong@gmail.com>
2026-01-01 21:13:39 +0800

    postgres db connection and docker setup

--- dag/news_pipeline.py
+++ dag/news_pipeline.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import logging
 import json
+import psycopg2
 
 log = logging.getLogger(__name__)
 
@@ -17,25 +18,51 @@ NEWS_API_LANGUAGE = os.getenv('NEWS_API_LANGUAGE')
 NEWS_API_TOPIC = os.getenv('NEWS_API_TOPIC')
 
 NEWS_API_TOP_HEADLINES_URL = os.getenv('NEWS_API_TOP_HEADLINES_URL')
-
+NEWS_CATEGORY = os.getenv('NEWS_CATEGORY')
 
 # fetching news data 
 news_api_everything_url = f"{NEWS_API_EVERYTHING_URL}{NEWS_API_KEY}{NEWS_API_LANGUAGE}{NEWS_API_TOPIC}&pageSize=5"
-news_api_top_headlines_url = f"{NEWS_API_TOP_HEADLINES_URL}{NEWS_API_KEY}&pageSize=5&category=technology&country=us"
+news_api_top_headlines_url = f"{NEWS_API_TOP_HEADLINES_URL}{NEWS_API_KEY}{NEWS_CATEGORY}&pageSize=5"
 
-def fetch_news():
+# fetch everything news
+def fetch_news_everything():
     response = requests.get(news_api_everything_url)
     response.raise_for_status()
     return response.json()
 
-# fetch_news({NEWS_API_URL}, {NEWS_API_KEY}, 'IBM', 'en')
+# fetch top headlines news
+def fetch_news_top_headlines():
+    response = requests.get(news_api_top_headlines_url)
+    response.raise_for_status()
+    return response.json()
+
+# connecting to Postgres database and save data
+def connect_to_postgres():
+    print("Connecting to PostgreSQL database...")
+    try:
+        connection = psycopg2.connect(
+            user=os.getenv('POSTGRES_USER'),
+            password=os.getenv('POSTGRES_PASSWORD'),
+            host=os.getenv('POSTGRES_HOST'),
+            port=os.getenv('POSTGRES_PORT'),
+            database=os.getenv('POSTGRES_DB')
+        )
+
+        print("Connection successful.")
+        return connection
+    except Exception as error:
+        print(f"Error connecting to PostgreSQL database: {error}")
+        return None, None
 
 if __name__ == "__main__":
     try:
-        news_data = fetch_news()
-        log.info("Fetched news data successfully.")
-        pretty = json.dumps(news_data, indent=2)
-        print(pretty)
-        log.info(pretty)
+        # news_everything_data = fetch_news_everything()
+        # log.info("Fetched news everything data successfully.")
+        # pretty = json.dumps(news_everything_data, indent=2)
+        # print(pretty)
+        # log.info(pretty)
+
+        print("Connecting to Postgres db")
+        connect_to_postgres()
     except Exception as e:
         log.error(f"Error fetching news data: {e}")
